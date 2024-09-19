6e44a2d120ebde30c510c00471dfc24bb7ec9230
Ahyar <a_maulana@mhs.unsyiah.ac.id>
2024-09-19 17:23:08 +0700

    tambah migration

---
+++ database/migrations/2024_09_12_073853_create_ruangan_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ruangan', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->id();
+            $table->foreignId('pic')->constrained('pic'); 
+            $table->integer('lantai');
+            $table->string('nama_ruangan');
+            $table->integer('kapasitas_ruangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ruangan');
+    }
+};
---
+++ database/migrations/2024_09_12_081556_create-booking-table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('booking', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('ruangan_id')->constrained('ruangan');
+            $table->string('nama_pengunjung');
+            $table->string('kontak_pengunjung');
+            $table->dateTime('waktu_pemakaian_awal');
+            $table->dateTime('waktu_pemakaian_akhir');
+            $table->dateTime('waktu_booking');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('booking');
+    }
+};
+
