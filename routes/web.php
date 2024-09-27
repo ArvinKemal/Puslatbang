f885c69757207d2b360a3d0c99945a77409569cb
Ahyar <a_maulana@mhs.unsyiah.ac.id>
2024-09-27 16:34:56 +0700

    +tvscreen

--- routes/web.php
+++ routes/web.php
@@ -74,3 +74,8 @@ Route::delete('/booking/{id}', [BookingController::class, 'destroy'])->name('boo
 
 // routing booking user
 Route::get('/booking-user', [BookingUserController::class, 'create'])->name('booking-user.add');
+
+// routing Tv Screen
+Route::get('/tv', function () {
+    return view('tvscreen');
+})->name('tv');
