eb50ed58051ae18a54ef9f69e458c53faf863e41
Arvin <arvin.kemal2017@gmail.com>
2024-10-02 00:50:01 +0700

    perbaikan ruangan-edit

--- app/Http/Controllers/Ruangan/RuanganController.php
+++ app/Http/Controllers/Ruangan/RuanganController.php
@@ -124,7 +124,8 @@ class RuanganController extends Controller
             'nama_ruangan' => 'required|string|max:255',
             'lantai' => 'required|integer',
             'kapasitas_ruangan' => 'required',
-            'pic' => 'required'
+            'pic_id' => 'required',
+            'image' => 'required|image|max:1000'
         ], [
             'nama_ruangan.required' => 'Nama wajib diisi',
             'nama_ruangan.string' => 'Nama harus berupa teks',
@@ -135,16 +136,23 @@ class RuanganController extends Controller
 
             'kapasitas_ruangan.required' => 'kapasitas ruangan wajib diisi',
 
-            'pic.required' => 'pic wajib diisi',
+            'pic_id.required' => 'pic wajib diisi',
+
+            'image.required' => 'image harus diisi',
+            'image.image' => 'image harus berupa jpg/jpeg/png',
+            'image.max:1000' => 'ukuran image maksimal 1000kb'
         ]);
 
         // Simpan data ke database
 
         $ruangan = Ruangan::findOrFail($id);
-        $ruangan->pic = $request->pic;
+        $ruangan->pic_id = $request->pic_id;
         $ruangan->lantai = $request->lantai;
         $ruangan->nama_ruangan = $request->nama_ruangan;
         $ruangan->kapasitas_ruangan = $request->kapasitas_ruangan;
+        $file = $request->file('image');
+        $path = Storage::putFile('public/ruangan', $file);
+        $ruangan->image = basename($path);
 
         $ruangan->save();
 
